25c4ff86b44f2428154ea78e3ad6bcba30568edc
hixon <ryan.hixon@gmail.com>
2023-08-07 19:04:36 -0400

    Add project files.

---
+++ sunday-service/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sunday_service.Models;
+
+namespace sunday_service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private ApplicationDbContext _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ApplicationDbContext ctx)
+        {
+            _logger = logger;
+            _context = ctx;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var test = _context.Players.ToList();
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ sunday-service/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sunday_service.Models
+{
+    [Table("test_player")]
+    public class Player
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public decimal startindex { get; set; }
+        public decimal currindex { get; set; }
+        public int typeid { get; set; }
+        public bool active { get; set; }
+        public DateTime createdt { get; set; }
+    }
+}
